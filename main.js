a3a5c0055da90d2ed6c545ee174280ca348e0f63
Soaring Radio <burroughszeb@me.com>
2016-11-02 20:05:55 -0500

    Init 2
    installed node.
    and setup electron

---
+++ main.js
@@ -0,0 +1,7 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+app.on('ready', () => {
+  let win = new BrowserWindow({width:1280, height:720})
+  win.loadURL(`file://${__dirname}/index.html`)
+})
